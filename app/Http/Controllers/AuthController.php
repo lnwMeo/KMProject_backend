d439f885ceeae9914afd0e39f4bd8aff9561e29c
lnwMeo <lnwmeo007@gmail.com>
2023-12-19 14:13:04 +0700

    edit Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\user;
+use App\Models\User;
 use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
